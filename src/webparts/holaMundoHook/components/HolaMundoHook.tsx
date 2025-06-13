c66a624aab9ce0763f899e9c06864db6e62343a5
SCM-NTT <scastrma@emeal.nttdata.com>
2025-06-13 13:29:27 +0200

    Adición de contador al componente Hook y scss para botón

--- src/webparts/holaMundoHook/components/HolaMundoHook.tsx
+++ src/webparts/holaMundoHook/components/HolaMundoHook.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
-import { useEffect } from 'react';
-//import styles from './HolaMundoHook.module.scss';
+import { useEffect, useState } from 'react';
+import styles from './HolaMundoHook.module.scss';
 import type { IHolaMundoHookProps } from './IHolaMundoHookProps';
 //import { escape } from '@microsoft/sp-lodash-subset';
 
 
 const useHolaMundoHook : React.FC<IHolaMundoHookProps> =({userDisplayName}) => {
+
+  const [count,setCount]= useState(0);
   
-  useEffect(()=>{console.log('Componente montado por:',userDisplayName);}, [userDisplayName]);
-  
+  const increment= ()=> setCount(prev => prev+1);
+  const decrement= ()=> setCount(prev => prev-1);
+  const zero =() => setCount(0);
 
 
-  return <h1>Hola Mundo por {userDisplayName}</h1>;
+  useEffect(()=>{
+    if(count%10 ===0){
+      console.log('El botón ha sido pulsado , ahora vale',count);
+    }
+  }, [count]);
+
+  return(
+    <div className={styles.container}>
+      <h1>Hola Mundo para {userDisplayName}</h1>
+      <b></b>
+      <p>Has pulsado el botón {count} veces.</p>
+      <button className={styles.button} onClick={increment}>+</button>
+      <button className={styles.button} onClick={zero}>reset</button>
+      <button className={styles.button} onClick={decrement}>-</button>
+    </div>
+  );
 }
   
 export default useHolaMundoHook;
